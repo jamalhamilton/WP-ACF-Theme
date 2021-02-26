e157f4fcc5ab1f63231ab77fe1ee3e010aaede65
jamal <eugenehamilton843@gmail.com>
2021-02-26 17:55:49 -0500

    show header

--- header.php
+++ header.php
@@ -81,7 +81,7 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" style="display: none;">
+		<nav id="site-navigation" class="main-navigation" >
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'amc-theme' ); ?></button>
 			<?php
 			wp_nav_menu(
