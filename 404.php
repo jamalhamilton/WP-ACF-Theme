1a8d86b2525ac97e234ad57374a99b5fc1866281
JackG <jack@accomplishagency.com>
2021-02-26 00:12:45 -0500

    Images

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
+		<section class="error-404 not-found  contained-width">
 			<header class="page-header">
 				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'amc-theme' ); ?></h1>
 			</header><!-- .page-header -->
