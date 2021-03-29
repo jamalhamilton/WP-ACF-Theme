90c1ea7d3a842d83904521fe4cd483dd69110e16
JackG <jack@accomplishagency.com>
2021-03-29 13:53:45 -0400

    Styles for modules

--- modules/module-featured_articles.php
+++ modules/module-featured_articles.php
@@ -1,5 +1,11 @@
 <!-- begin main wrapper for modules -->
  <div class="wrapper">
+   <h3 class="vertcial_text">
+       <?php
+           //
+           the_sub_field("vertical_title");
+       ?>
+   </h3>
  <!-- header -->
 	<header>
         <h1 class="module-header">
@@ -8,12 +14,6 @@
                 the_sub_field("main_headline_copy");
             ?>
         </h1>
-        <h3 class="vertcial_text">
-            <?php
-                //
-                the_sub_field("vertical_title");
-            ?>
-        </h3>
 	</header>
  <!-- end header -->
 
@@ -43,11 +43,12 @@
                     <?php echo $subtitle; ?>
                 </h2>
             </a>
-            <a href="#" class="read-more nodec"><?php echo $cta; ?>
+            <a href="<?php echo $cta['url']; ?>" class="read-more nodec"><?php echo $cta['title']; ?>
             <svg xmlns="http://www.w3.org/2000/svg" width="59.382" height="22.001" viewBox="0 0 59.382 22.001"><defs><style>.e{fill:<?php echo $text_color;?>;stroke-width: 2px;stroke: <?php echo $text_color;?>;}</style></defs><path class="e" d="M64.359,40.636h-53.8l7.646-7.646a1.033,1.033,0,1,0-1.461-1.461l-9.3,9.3a1.03,1.03,0,0,0-.439.843c0,.005,0,.01,0,.016s0,.022,0,.033a1.025,1.025,0,0,0,.056.289,1,1,0,0,0,.075.165,1.037,1.037,0,0,0,.088.132.97.97,0,0,0,.079.119l9.5,9.5a1.033,1.033,0,1,0,1.461-1.461L10.515,42.7H64.359a1.033,1.033,0,0,0,0-2.067Z" transform="translate(65.892 52.727) rotate(180)"/></svg>
             </a>
         </div>
+        <?php endwhile; ?>
     </section>
-    <?php endwhile; ?>
+
     <?php endif; ?>
 </div>
